f49a99e8fe1ce5d68ce2cb0cea679b1549836b1d
jennifer-Arciniegas <jennyarciniegas-28@hotmail.com>
2025-01-06 10:10:24 -0500

    ejercicios en casa

---
+++ stuctures/conditions.js
@@ -0,0 +1,8 @@
+let name = prompt("ingrese el nombre")
+let age = parseInt(prompt("ingrese la edad"));
+
+if (age >= 18){
+    console.log("eres mayor de edad")
+}else{
+    console.log("eres menor")
+}
